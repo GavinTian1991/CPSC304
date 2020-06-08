2bedb108ed051e67362151eb0665c77e11752d7d
GavinTian1991 <tian.xiaojun1991@gmail.com>
2020-06-07 21:19:28 -0700

    Add rating level select

--- inc/customer.php
+++ inc/customer.php
@@ -119,6 +119,16 @@
                     </select>
                 </div>
                 <div class="col-sm">
+                    <select name="ratinglevel" class="custom-select">
+                        <option selected="">Rating Above</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
+                </div>
+                <div class="col-sm">
                     <select name="drinktype" class="custom-select">
                         <option selected="">Like Drink Type</option>
                         <option value="1">Cream Cap Tea</option>
