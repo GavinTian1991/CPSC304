21b898c0a2ca17541a74cf0484b036d37e08ae22
GavinTian1991 <tian.xiaojun1991@gmail.com>
2020-06-16 21:56:27 -0700

    Change timezone to vancouver

--- inc/config/db.php
+++ inc/config/db.php
@@ -3,6 +3,8 @@
      require('config.php');
 
 	 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS,DB_NAME) or die("Connect failed: %s\n". $conn -> error);
+
+	 date_default_timezone_set('America/Vancouver');
 	 
 	 if(mysqli_connect_errno()) {
 		 echo 'Failed to connect to Mysql'. mysqli_connect_errno();;
